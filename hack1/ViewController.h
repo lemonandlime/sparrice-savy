55b04d07ef9be3a139259afba980bbf809c2daa9
Karl Söderberg <karl.soderberg@gmail.com>
2014-11-28 18:06:28 +0100

    Initial Commit

---
+++ hack1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hack1
+//
+//  Created by Karl Söderberg on 28/11/14.
+//  Copyright (c) 2014 Sparrice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hack1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  hack1
+//
+//  Created by Karl Söderberg on 28/11/14.
+//  Copyright (c) 2014 Sparrice. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
